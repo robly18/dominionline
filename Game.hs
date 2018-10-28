81c1fa2579a1f1500b43a22bad52e03493df04be
robly18 <duarteluis.nascimento@hotmail.com>
2018-10-28 18:12:56 +0000

    finished implementing game.hs

--- Game.hs
+++ Game.hs
@@ -1,16 +1,23 @@
 {-# LANGUAGE DeriveGeneric #-}
---module Game (newGame, act, State, encode, decode, joinGame,
---            Action (Write, Start, Poll)) where
+{-# LANGUAGE TemplateHaskell #-}
+module Game (newGame, act, State, encode, decode, joinGame,
+            Action (Request, Start, Poll)) where
 
 import Prelude hiding (log)
 
-import Data.Sequence
+import qualified Data.Sequence as S
 import Data.Maybe
-import Data.Aeson (ToJSON, FromJSON)
+import Data.Aeson (encode, decode, ToJSON, FromJSON)
+import Data.List
+import Data.Foldable
 
 import Control.Monad
+import Control.Monad.Random.Lazy (lift, RandT, StdGen)
+import System.Random.Shuffle
 
 import GHC.Generics
+
+import Control.Lens
 
 type Deck = [Int]
 
@@ -25,10 +32,11 @@ instance FromJSON Action
 
 type PlayerState = (Deck, [Int]) --Hand, Points made
 
-data Table = Table {players :: Seq PlayerState, playing :: Int, pool :: Deck}
+data Table = Table {_players :: S.Seq PlayerState, _playing :: Int, _pool :: Deck}
     deriving (Generic, Show)
 instance ToJSON Table
 instance FromJSON Table
+makeLenses ''Table
 
 data Log a = Log [String] a
     deriving (Generic, Show)
@@ -43,6 +51,8 @@ instance Monad Log where
     (Log l1 x) >>= f = let Log l2 y = f x in Log (l1 ++ l2) y
 log :: String -> Log ()
 log s = Log [s] ()
+
+type RL = RandT StdGen Log
 
 data State = State Table
            | StartState Int
@@ -60,16 +70,58 @@ joinGame (StartState plrno) = do log $ "Player "++show plrno++" joins the game!"
 joinGame s = return (Nothing, s)
 
 
-act :: State -> (Int, Action) -> Log State
+act :: State -> (Int, Action) -> RL State
 act st (_, Poll) = return st
-act (State t) (plr, Request card) = (fmap State) $ request plr card t
+act (State t) (plr, Request card) = lift $ (fmap State) $ request plr card t
 act s@(StartState plrno) (plr, action) = case action of
-    Start -> (log $ "Player "++show plr++" has started the game." ) >> (return $ State $ newTable plrno)
+    Start -> do lift $ log $ "Player "++show plr++" has started the game."
+                nt <- newTable plrno
+                return $ State nt
     _     -> return s
 act s (_, _) = return s
 
 request :: Int -> Int -> Table -> Log Table
-request _ _ _ = undefined
+request plr card t@(Table tplayers _ _) =
+     do let playersWhoHave = S.findIndicesL (elem card . fst) tplayers
+        if plr `elem` playersWhoHave then
+             do log $ "Player "++show plr++" requested "++show card++"."
+                let others = delete plr playersWhoHave
+                if others == [] then (log "But nobody has it...") >> goFish plr card t
+                else do log $ show playersWhoHave --placeholder
+                        let collected = [ c | (p,_) <- toList tplayers, c <- p, c == card]
+                        let withoutCollected = over players (fmap $ over _2 $ filter (/= card)) t
+                        log $ "They collected "++show (length collected)++" cards!"
+                        return $ over players (S.adjust (over _2 $ (collected++)) plr) t
+        else return t
+        
 
-newTable :: Int -> Table
-newTable _ = undefined
+nextPlr :: Table -> Log Table
+nextPlr t = do let newPlr = ((view playing t) + 1) `mod` (length $ view players t)
+               log $ "It's player "++show newPlr++"'s turn."
+               return $ set playing newPlr t
+
+goFish :: Int -> Int -> Table -> Log Table
+goFish plr req t =   do let tpool = view pool t
+                        case tpool of
+                          [] -> nextPlr t
+                          (x:xs) ->
+                            do log "Go fish!"
+                               let fished = set pool xs $
+                                            over players (S.adjust (over _2 $ (x:)) plr) $
+                                            t
+                               if x == req then
+                                 do log $"They got a "++show req++"! It's their turn again"
+                                    return fished
+                               else
+                                 do log $ "Tough luck. It wasn't the card they wanted."
+                                    nextPlr t
+                                    
+
+newTable :: Int -> RL Table
+newTable plrno = do lift $ log $ "The game has started!"
+                    decklist <- shuffleM $ deck 10
+                    let (players, tpool) = extractPlayers plrno decklist
+                    return $ Table (S.fromList players) 0 tpool
+        where extractPlayers 0 decklist = ([], decklist)
+              extractPlayers n decklist = let (pl, nd) = extractPlayers (n-1) decklist in
+                                            ((take 4 nd,[]):pl, (drop 4 nd))
