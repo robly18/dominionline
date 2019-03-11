780b4da8446dbb68b1f62a93ee0dd14fd69bbb27
robly18 <duarteluis.nascimento@hotmail.com>
2019-03-11 18:15:00 +0000

    basic text chat

--- Game.hs
+++ Game.hs
@@ -19,5 +19,45 @@ import GHC.Generics
 
 import Control.Lens
 
---todo :(
+data Log a = Log [String] a
+    deriving (Generic, Show)
+instance (ToJSON a) => ToJSON (Log a)
+instance (FromJSON a) => FromJSON (Log a)
+instance Functor Log where
+    fmap f (Log l x) = Log l $ f x
+instance Applicative Log where
+    pure x = Log [] x
+    (Log l1 f) <*> (Log l2 x) = Log (l1 ++ l2) (f x)
+instance Monad Log where
+    (Log l x) >>= f = let Log l2 y = f x in Log (l ++ l2) y
+log :: String -> Log ()
+log s = Log [s] ()
+forget :: Log a -> a
+forget (Log _ x) = x
+
+type RL = RandT StdGen Log
+
+data State = State Int
+    deriving (Generic, Show)
+
+newGame :: State
+newGame = State 0
+
+instance ToJSON State
+instance FromJSON State
+
+data Action = Poll | Say String
+    deriving (Generic, Show)
+instance ToJSON Action
+instance FromJSON Action
+
+act :: State -> (Int, Action) -> RL State
+act s (plr, a) = case a of
+                    Poll -> return s
+                    Say x -> do lift $ log $ show plr ++ ": " ++ x
+                                return s
+
+joinGame :: State -> Log (Maybe Int, State)
+joinGame (State s) = do log $ "Player " ++ show s ++ " joins."
+                        return (Just s, State $ s+1)
 
